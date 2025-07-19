4c8d91c98645bcb50487eed424aeb3e8555ed033
Yousef Korayem <yousefkorayem1@gmail.com>
2025-07-19 15:03:13 +0300

    adjusted sidebar components

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 import './Sidebar.scss';
 
+import cart from '../../assets/cart.svg';
+
 const Sidebar = ({ isOpen, onClose }) => {
   return (
     <div className={`sidebar ${isOpen ? 'sidebar--open' : ''}`}>
-      <button className="sidebar__close" onClick={onClose}>×</button>
-      <button>Login</button>
-      <button>Sign Up</button>
-      <button>Cart</button>
+        <button className="sidebar__close" onClick={onClose}>×</button>
+        <button className="sidebar__nav--login">Log In</button>
+        <button className="sidebar__nav--signup">Sign Up</button>
+        <button className="sidebar__nav--cart"><img className="sidebar__nav--cart--icon" src={cart}/></button>
+        
     </div>
   );
 };
